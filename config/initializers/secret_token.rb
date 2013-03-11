6db44b261d91600a9be60d5b4646771b7cc173dc
Ash Ganatra <ash@appnotiq.com>
2013-03-11 19:59:32 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TurntNinja::Application.config.secret_token = '31d4194b56f880ad6ff6e3f84b38f503e869bcdebd0a2812fca7760dfc4e6170e0ef59f9be9ecc0d8121291776c500d3c117ddfe22a247856f56bc80b87ee6da'
